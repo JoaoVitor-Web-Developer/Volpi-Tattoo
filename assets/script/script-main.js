b017e757c4d9907fe8366b217f92c31c16cc03f9
João Vitor Targino De Oliveira <119077083+JoaoVitor-Web-Developer@users.noreply.github.com>
2022-12-29 22:49:21 -0300

    Add files via upload

--- assets/script/script-main.js
+++ assets/script/script-main.js
@@ -23,25 +23,6 @@ function closeFullImg(){
 
 // ============================================ //
 
-function sendEmail() {
-  Email.send({
-      SecureToken : 'ee2b918a-0756-4843-a48d-90c3fd001921',
-      To : 'volpitattoo@outlook.com',
-      From : document.getElementById("email").value,
-      Subject : "Novo E-mail de contato recebido",
-      Body : "Name: " + document.getElementById("name").value
-          + "<br> Email: " + document.getElementById("email").value
-          + "<br> Telefone: " + document.getElementById("phone").value
-          + "<br> Mensagem: " + document.getElementById("message").value
-          
-  }).then(
-    message => alert("E-mail Enviado Com Sucesso")
-  );
-}
-
-
-// ============================================ //
-
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px"
   document.getElementById("main").style.marginLeft = "250px"
